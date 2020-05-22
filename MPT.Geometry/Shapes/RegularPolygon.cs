cd242064e50d7d7cbccb39905592fb270c932ff1
Mark P Thomas <markums@gmail.com>
2020-05-21 23:40:36 -0600

    Setting up for CI

--- MPT.Geometry/Shapes/RegularPolygon.cs
+++ MPT.Geometry/Shapes/RegularPolygon.cs
@@ -1,16 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MPT.Geometry.Shapes
 {
     /// <summary>
     /// Class RegularPolygon.
-    /// Implements the <see cref="MPT.Geometry.Area.Polygon" />
+    /// Implements the <see cref="MPT.Geometry.Shapes.Polygon" />
     /// </summary>
-    /// <seealso cref="MPT.Geometry.Area.Polygon" />
+    /// <seealso cref="MPT.Geometry.Shapes.Polygon" />
     public class RegularPolygon : Polygon
     {
         
